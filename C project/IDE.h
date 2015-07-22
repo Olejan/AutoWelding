83ec96360e8a66535513d794f863fef01415f7eb
gos <buran1td@mail.ru>
2015-07-22 23:47:47 +0300

    Create repositiry

---
+++ C project/IDE.h
@@ -0,0 +1,22 @@
+#include "typedef.h"
+#include <avr/io.h>      // Определяет имена для портов ввода-вывода
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+//#include <avr/eeprom.h>
+#include <stdio.h>       // подключение модуля ввода-вывода
+//#include <stdbool.h>
+#include <util/delay.h>	 // библиотека задержек
+
+#include "InlineFunctions.h"
+
+
+#ifdef WDT_ENABLE
+extern void SetWdtTimeLabel();
+extern void ResetWdtTimeLabel();
+extern BOOL CheckWdtTime(u32 time); // Chech, Reset And Note WdtTime
+#endif
+extern void wdt_start(u8 level);
+extern void wdt_stop();
+extern void wdt_feed();
+extern void ResetProc();
+
